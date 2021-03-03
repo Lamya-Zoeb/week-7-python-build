6ba9f958ed0e94c66d955f8218830c24b4cb6a5a
Lamya-Zoeb <lamyaaazoeb18@gmail>
2021-03-03 14:58:41 -0500

    Writing our first Python script

---
+++ Variables.py
@@ -0,0 +1,23 @@
+# this is a comment
+#this is a print method. Print will echo out anything inside the
+# () round brackets to the terminal, when we run the file
+print("This is my very first python script!!!")
+
+# variables are placeholders, with dynamic values- Things that can change
+#they get stored in memory and referenced later
+name = "Lamya"
+age = 19
+eyecolor = "brown"
+haircolor = "brown"
+
+#This is an array-- they are variables on steriods. They let us store many values in one
+#variable-> to help us group data.
+#This makes our scripting files easier to understand and work it.
+cars = ["BMW", "Porche", "Mercedes"]
+
+print("My name is " + name)
+
+name = input("What is YOUR name?")
+print("My name is now " + name)
+
+print("My age is " + str(age))
